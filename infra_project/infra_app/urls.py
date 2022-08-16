e6af7b63ffab230162f2be292b3633036a8222df
Tatyana <tatyana.arch@mail.ru>
2022-08-16 16:38:34 +0600

    1 коммит

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'infra_app'
 
+# удалить потом
 urlpatterns = [
     path('', views.index, name='index'),
     path('second/', views.second_page, name='second_page'),
